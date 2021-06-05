ef9175d6763980af5a8a97544edf3bba4e9fecb7
[Eskin Dmitriy] <9255151@mail.ru>
2021-06-05 21:07:27 +0300

    добавил задачи 4-5 и начал работу над 5 задачей

---
+++ Lesson2/5/Lesson2Work5/Lesson2Work5/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lesson2Work5
+{
+    class Program
+    {
+        enum WinterMonth
+        {
+            Декабрь = 0b_001,
+            Январь = 0b_010,
+            Февраль = 0b_100,
+        }
+        enum temp 
+        { 
+            
+        }
+
+        static void Main(string[] args)
+        {
+            int check1;
+            int check2;
+            int check3;
+            Console.Write("Введите порядковый номер месяца: ");
+            string NumberMonth = Console.ReadLine();
+            Console.Write("Введите максимальную температуру: ");
+            string MaxTemp = Console.ReadLine();
+            Console.Write("Введите минимальную температуру: ");
+            string MinTemp = Console.ReadLine();
+            bool result1 = int.TryParse(NumberMonth, out check1);
+            bool result2 = int.TryParse(MaxTemp, out check2);
+            bool result3 = int.TryParse(MinTemp, out check3);
+            if (result1 && result2 && result3)
+            {
+                Console.Write("удача");
+            }
+            else
+            {
+                Console.Write("Введите целое число!!!");
+            }
+             
+              
+
+            
+            
+            
+        
+        }
+    }
+}
