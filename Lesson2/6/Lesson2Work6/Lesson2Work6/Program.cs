e8e7820ba9f62a59a2a667d70314ad353dad015d
[Eskin Dmitriy] <9255151@mail.ru>
2021-06-06 21:22:37 +0300

    добавил урок номер 6

---
+++ Lesson2/6/Lesson2Work6/Lesson2Work6/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace Lesson2Work6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int check;
+            Console.Write("Введите число (палиндром): ");
+            string result = Console.ReadLine();
+            if(string.IsNullOrEmpty(result))
+            {
+                Console.Write("Значение не введено, программа завершает работу");
+            }
+            else
+            {
+                bool BoolResult = int.TryParse(result, out check);
+                if(BoolResult)
+                {
+                    int IntResult = Convert.ToInt32(result);
+                    if (isPalindrom(IntResult))
+                    {
+                        Console.Write("Число палиндром");
+                    }
+                    else
+                    {
+                        Console.Write("Число не палиндром");
+                    }
+                }
+            }           
+
+            bool isPalindrom(int num)
+            {
+                string str = Convert.ToString(num);
+                bool res = true;
+                for (int n = 0; n < str.Length; n++)
+                {                    
+                    if (result[n] != result[result.Length - n - 1])
+                    {
+                        res = false;
+                    }
+                }
+                return res;
+            }            
+        }
+    }
+}
