37dc96591407f63cc219ecfc19ffe9a91757bc7e
[Eskin Dmitriy] <9255151@mail.ru>
2021-06-08 12:38:50 +0300

    добавил задание номер 4

--- Lesson2/4/Lesson2Work4/Lesson2Work4/Program.cs
+++ Lesson2/4/Lesson2Work4/Lesson2Work4/Program.cs
@@ -6,7 +6,34 @@ namespace Lesson2Work4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int INN = 6451;
+            DateTime DateAndTime = new DateTime(2021, 2, 3, 21, 36, 33);
+            string Fuel = "АИ-95";
+            string Org = "ООО Альфа Трейд";
+            int V = 50;
+            double Cost = 35.20;
+            double result = V * Cost;
+            Console.WriteLine($"               {Org}");
+            Console.WriteLine("        Добро пожаловать ! АЗС № 127");
+            Console.WriteLine("           МЫ ГАРАНТИРУЕМ КАЧЕСТВО");
+            Console.WriteLine("Наличный расчет");
+            Console.WriteLine($"ИНН 0000{INN}");
+            Console.WriteLine(DateAndTime);
+            Console.WriteLine("ПРОДАЖА");
+            Console.WriteLine(Fuel + " (2-ТРК)");
+            Console.WriteLine("Наличный расчет");
+            Console.WriteLine($"                     {V}.00 x {Cost}");
+            Console.WriteLine("                         ={0:0.00}", result);
+            Console.WriteLine("ИТОГ" + "                    =" + " {0:0.00}", result);
+            Console.WriteLine("Наличными" + "               =" + " {0:0.00}", result);
+            Console.WriteLine("Сдача" + "                      =" +  " 0.00");
+            Console.WriteLine("            СПАСИБО ЗА ПОКУПКУ");
+            Console.WriteLine("            СЧАСТЛИВОГО ПУТИ!");
+            
+
+
+
+
         }
     }
 }
---
+++ Lesson2/6/Lesson2Work6/Lesson2Work6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson2Work6
+{
+    class Program
+    {
+        enum DayWeek
+        {
+            Понедельник = 0b_1000000,
+            Вторник = 0b_0100000,
+            Среда = 0b_0010000,
+            Четверг = 0b_0001000,
+            Пятница = 0b_0000100,
+            Суббота = 0b_0000010,
+            Воскресенье = 0b_0000001,
+        }
+        static void Main(string[] args)
+        {
+            DayWeek Office1 = DayWeek.Вторник | DayWeek.Среда | DayWeek.Четверг | DayWeek.Пятница;
+            DayWeek Office2 = DayWeek.Понедельник | DayWeek.Вторник | DayWeek.Среда | DayWeek.Четверг | DayWeek.Пятница | DayWeek.Суббота | DayWeek.Воскресенье;
+            Console.Write("Введите офис который Вам нужен; ");
+            string Office = Console.ReadLine();
+            if(string.IsNullOrEmpty(Office))
+            {
+                Console.WriteLine("День недели введен не верно!!!");
+            }
+            else
+            {
+                switch (Office)
+                {
+                    case "Офис1:":
+                       DayWeek resul1 = DayWeek.Понедельник & DayWeek;
+                        break;
+                       
+                 
+
+
+                }
+            }
+                
+           
+
+        }
+    }
+}
